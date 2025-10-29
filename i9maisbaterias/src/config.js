42ec517c2cd3000eb3f8362b807910feb79c7ed2
teqqzz <lucastequizu@gmail.com>
2025-10-29 19:11:40 -0300

    modified:   i9maisbaterias/src/config.js

--- i9maisbaterias/src/config.js
+++ i9maisbaterias/src/config.js
@@ -1,3 +1,7 @@
-// Constante que aponta para seu backend.
-export const API_URL = import.meta.env.VITE_API_URL || '';
+// Pega a URL pública principal definida nas variáveis de ambiente da Vercel
+const PUBLIC_PRODUCTION_URL = import.meta.env.VITE_PUBLIC_PRODUCTION_URL || ''; 
 
+// Se estiver em DEV local, usa localhost. 
+// Se estiver em PROD (Vercel), usa a URL pública definida.
+// Se a URL pública não for definida na Vercel, usa caminho relativo como fallback.
+export const API_URL = import.meta.env.VITE_API_URL /* localhost */ || PUBLIC_PRODUCTION_URL /* URL do domínio */ || ''; /* fallback relativo */
